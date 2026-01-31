84a5180ec00b0748d1f77d9d1a9c9ab17b7f95fa
Bhanuprasad0079 <akabhanuprasad@gmail.com>
2026-01-31 15:40:42 +0530

    Update layout and integrate latest changes

--- GMS-3.0/client/src/app/layout.tsx
+++ GMS-3.0/client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from '@/component/Navbar'
-
+import Navbar from '@/component/Navbar';
+import Watermark from '@/component/Watermark'; // <--- Import the new component
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-                <Navbar></Navbar>
-
+        <Watermark /> {/* <--- Add the Watermark here */}
+        <Navbar />
         {children}
       </body>
     </html>
