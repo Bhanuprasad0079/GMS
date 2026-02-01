6e55ec4cd50bfc00c6b4d0c5e22bccf67d163749
Bhanuprasad0079 <akabhanuprasad@gmail.com>
2026-02-02 01:03:48 +0530

    environment variable update

--- GMS-3.5/client/src/utils/api.ts
+++ GMS-3.5/client/src/utils/api.ts
@@ -2,5 +2,4 @@
 // based on where the code is running.
 
 // export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5087";
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-export const API_BASE_URL = "http://localhost:5087";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
